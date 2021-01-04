ae73130bcd594c83e538c8f4d86cd34bbe117891
Pratik Dhoke <pratikdhoke13@gmail.com>
2021-01-04 12:33:57 +0530

    update app.py
    data passing testing through serial communication

--- web_app/app.py
+++ web_app/app.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template
+from datetime import datetime
+import qr_flask 
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+	now = datetime.now()
+	d1 = now.strftime("%d-%m-%Y")
+	t1 = now.strftime("%H:%M:%S")
+	w1 = qr_flask.weight()
+	return render_template('index.html',dt = d1, t1=t1, w1=w1)
